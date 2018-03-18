1f798adb52b5e5ca1762ace2da070ade092e3a02
Anil Kumar Yadav <anilyadv2014@gmail.com>
2018-03-18 22:24:28 +0530

    Add files via upload

---
+++ anil_soln/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import pyrebase
+import bs4 as bs
+import urllib.request
+import numpy as np
+import pandas as pd
+
+
+config = {
+    'apiKey': "AIzaSyDmyDz3GpxEkIZnrauTbSkSGthZk2LHNX4",
+    'authDomain': "anil-soln.firebaseapp.com",
+    'databaseURL': "https://anil-soln.firebaseio.com",
+    'projectId': "anil-soln",
+    'storageBucket': "anil-soln.appspot.com",
+    'messagingSenderId': "1005850294597"
+  }
+
+firebase = pyrebase.initialize_app(config)
+
+# Get a reference to the database service
+db = firebase.database()
+
+all = {}
+a = db.child('/').get()
+for user in a.each():
+    c = user.key()
+    d = user.val()
+    all.update({c:d})
+
+# Create your views here.
+def index(request):
+    return render(request, 'anil_soln/index.html', {'dic2': all})
